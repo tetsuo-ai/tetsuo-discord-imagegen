7de50ecd7892606bde55d8060cf61381726bb40c
Jon Bowie <jon.bowie@gmail.com>
2025-01-02 12:03:38 -0500

    Large rewrite of image command parser, most of the bugs are fixed, help output and bad command parsing need to be addressed.  ASCII image creation is being addressed.  New command parser code should be 🔥

--- image_processing/config/presets.py
+++ image_processing/config/presets.py
@@ -2,35 +2,6 @@
 
 # from typing import Dict, Any
 
-EFFECT_ORDER = [
-    "rgb",
-    "color",
-    "glitch",
-    "chroma",
-    "scan",
-    "noise",
-    "energy",
-    "pulse",
-    "consciousness",
-]
-
-EFFECT_PRESETS = {
-    "glitch": {"intensity": 0.5},
-    "chroma": {"intensity": 0.5},
-    "scan": {"gap": 2, "opacity": 0.5},
-    "noise": {"intensity": 0.5},
-    "energy": {"intensity": 0.5},
-    "pulse": {"intensity": 0.5},
-    "consciousness": {"intensity": 0.5},
-    "g": {"intensity": 0.5},
-    "b": {"intensity": 0.5},
-    "impact": {"intensity": "$TETSUO", "font": 70},
-    "frames": {"count": 24},
-    "fps": {"count": 6},
-    "rgb": {"r": 75, "g": 75, "b": 75},
-    "rgbalpha": {"insensity": 255},
-}
-
 ANIMATION_PRESETS = {
     "cyberpunk": {
         "params": {
